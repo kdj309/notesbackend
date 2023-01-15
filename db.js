a77bb0d2fb7b0d3ff59211fceac5e3a33991961d
kartik <karthik.joshi103@gmail.com>
2023-01-15 14:53:21 +0530

    changed mongo uri

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
 //importing mongoose library which is used to ease the databse related work by providing built in methods such as schema
-const mongoUri = "mongodb://localhost:27017/Notes";
+const mongoUri = "mongodb+srv://kdj:6r1Rh9ijEdKurIVS@cluster0.f6cok.mongodb.net/?retryWrites=true&w=majority";
 //unique string of the localhost mongodb databse
 const dbconnect = () => {
     mongoose.connect(mongoUri, () => {
